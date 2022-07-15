e0593dd2e5a28342df08228a37e8f96329272f46
Cody Robertson <codyallanrobertson@gmail.com>
2022-07-14 23:05:36 -0500

    fix foreign keys

--- api/project/models.py
+++ api/project/models.py
@@ -17,14 +17,16 @@ class BudgetAmount(db.Model):
     month = db.Column(db.Integer)
     year = db.Column(db.Integer)
     amount = db.Column(db.Numeric(precision=15, scale=2, asdecimal=True))
+    category_id = db.Column(db.Integer, db.ForeignKey("budget_category.id"))
     category = db.relationship(
-        "BudgetCategory", backref=db.backref("budget_amounts", lazy=True)
+        "budget_category", backref=db.backref("budget_amounts", lazy=True)
     )
 
 
 class BudgetItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     value = db.Column(db.Numeric(precision=15, scale=2, asdecimal=True))
+    category_id = db.Column(db.Integer, db.ForeignKey("budget_category.id"))
     category = db.relationship(
-        "BudgetCategory", backref=db.backref("budget_items", lazy=True)
+        "budget_category", backref=db.backref("budget_items", lazy=True)
     )
